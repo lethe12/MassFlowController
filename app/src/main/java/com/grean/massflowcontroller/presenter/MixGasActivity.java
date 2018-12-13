65e1a56c1b2566dee26c65004d90c32387a41c03
lethe12 <weifeng_apex@126.com>
2018-12-13 16:34:27 +0800

    Initial commit

---
+++ app/src/main/java/com/grean/massflowcontroller/presenter/MixGasActivity.java
@@ -0,0 +1,18 @@
+package com.grean.massflowcontroller.presenter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by weifeng on 2018/12/13.
+ */
+
+public class MixGasActivity extends Activity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
